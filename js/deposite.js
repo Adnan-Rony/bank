6337828e22aa1c8b829ec2b25d2d323cb15224ea
Adnan-Rony <adnanrony19@gmail.com>
2023-08-15 13:10:04 +0600

    withdraw

--- js/deposite.js
+++ js/deposite.js
@@ -1,12 +1,56 @@
     document.getElementById('btn-deposit').addEventListener('click',function(){
     const depositField= document.getElementById('Deposit-field');
     //    
-    const depositeAmount=depositField.value;
+    const NewDepositeAmountString=depositField.value;
+    const NewDepositeAmount=parseFloat(NewDepositeAmountString);
+
     // console.log(depositeAmount);
 
     //    
     const depositTotalElement= document.getElementById('deposit-total');
-    const depositTotal=depositTotalElement.innerText;
-  depositTotalElement.innerText =depositeAmount
+
+    const previousDepositTotalString=depositTotalElement.innerText;
+    const previousDepositTotal=parseFloat(previousDepositTotalString);
+
+        const currentDepositTotal=previousDepositTotal+ NewDepositeAmount;
+
+
+
+  
+  depositTotalElement.innerText =currentDepositTotal;
+
+  //clear
+  depositField.value='';
+
+        //balance add
+     const balanceTotalElement  = document.getElementById('Balance-total');
+     const previousBalanceTotalString=balanceTotalElement.innerText;
+
+     const previousBalanceTotal=parseFloat(previousBalanceTotalString);
+
+    //  calculate
+
+    const cureenBalanceTotal=previousBalanceTotal + NewDepositeAmount;
+
+    //set the balance total
+    balanceTotalElement.innerText=cureenBalanceTotal;
+
+            // // new
+            // document.getElementById('btn-withdraw').addEventListener('click',function(){
+
+            //     const WithdrawField=document.getElementById('withdraw-field');
+            //     const NewWithdrawamountString=WithdrawField.value;
+            //    const NewWithdrawamount=parseFloat(NewWithdrawamountString);
+               
+
+            //    const withdrawTotaleLement=document.getElementById('withdraw-total');
+            //    const previousWithdrawTotalString=withdrawTotaleLement.innerText;
+
+            //    const previousWithdrawTotal=parseFloat(previousWithdrawTotalString);
+            //    const CurrentWithdrawTotal=previousWithdrawTotal+NewWithdrawamount;
+
+            //    withdrawTotaleLement.innerText=CurrentWithdrawTotal;
+            // })
+
 
     })
---
+++ js/withdraw.js
@@ -0,0 +1,65 @@
+/* 1.add event handler with withdraw button
+
+ 2.get the withdraw amount from the withdraw input field
+
+2.5:also make sure to convert the input into a number by using parseFloat
+
+3.get previous withdraw total
+
+4.calculate total withdraw amount
+
+5.set total withdraw amount
+
+6.clear the input
+
+7.get the previous balance total
+
+8.calculate new balance total
+
+*/
+
+
+
+// step-1:
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    
+    // step-2:
+    const withdrawfield=document.getElementById('withdraw-field');
+    const NewWithdrawAmountString=withdrawfield.value ;
+    const NewWithdrawAmount=parseFloat(NewWithdrawAmountString);
+    // console.log(NewWithdrawAmount);
+
+
+    //step-3:
+    const withdrawTotaleLement=document.getElementById('withdraw-total');
+    const previousWithdrawTotalString=withdrawTotaleLement.innerText;
+    const previousWithdrawTotal=parseFloat(previousWithdrawTotalString);
+    console.log(previousWithdrawTotal);
+
+    // step-4:
+
+    const CurrentWithdrawTotal=previousWithdrawTotal+ NewWithdrawAmount;
+
+    //step-5;
+
+    withdrawTotaleLement.innerText=CurrentWithdrawTotal
+
+
+    // step-6
+    withdrawfield.value='';
+
+    // step-7:
+
+    const balanceTotalElement=document.getElementById('Balance-total');
+    const previousBalanceTotalString=balanceTotalElement.innerText;
+    const previousBalanceTotal=parseFloat(previousBalanceTotalString);
+
+    // step-8
+    const newBalanceTotal=previousBalanceTotal-NewWithdrawAmount;
+    balanceTotalElement.innerText=newBalanceTotal
+
+
+
+
+
+})
